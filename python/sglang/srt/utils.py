e3f52c22c635b78d30fdabd88d24a50a03096f59
CaoE <e.cao@intel.com>
2025-07-29 16:21:36 +0000

    minor change

--- python/sglang/srt/utils.py
+++ python/sglang/srt/utils.py
@@ -209,12 +209,16 @@ try:
     is_intel_amx_backend_available = hasattr(
         torch.ops.sgl_kernel, "convert_weight_packed"
     )
+except:
+    is_intel_amx_backend_available = False
+
+
+try:
     # move torch._C._cpu._is_amx_tile_supported() from cpu_has_amx_support
     # to support torch compile
     is_amx_tile_supported = torch._C._cpu._is_amx_tile_supported()
 except:
-    is_intel_amx_backend_available = False
-    is_amx_tile_supported = torch._C._cpu._is_amx_tile_supported()
+    is_amx_tile_supported = False
 
 
 def cpu_has_amx_support():
